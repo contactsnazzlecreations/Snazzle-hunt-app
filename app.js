f76750741b93e128b6e1978aaf32a99687177724
contactsnazzlecreations <contact.snazzlecreations@gmail.com>
2026-08-22 23:59:28 +0200

    Load automatic shop email modules

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Snazzle Hunt entrypoint: keep the proven hunt app core separate from newer modules.
 import './app-core.js';
-import './shop-compat.js?v=6';
+import './shop-compat.js?v=7';
 import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js';
 
 // Load the shop only after Firebase has restored/created a signed-in user.
@@ -10,7 +10,9 @@ let shopLoaded = false;
 onAuthStateChanged(auth, user => {
   if (!user || shopLoaded) return;
   shopLoaded = true;
-  import('./shop.js?v=6')
-    .then(()=>import('./shop-email-settings.js?v=6'))
+  import('./shop.js?v=7')
+    .then(()=>import('./shop-email-settings.js?v=7'))
+    .then(()=>import('./shop-mail-setup.js?v=7'))
+    .then(()=>import('./shop-mail-queue.js?v=7'))
     .catch(err => console.error('Snazzle shop kon niet laden', err));
 });
