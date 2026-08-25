57ac06883e31ea7941d8a1cd684e5d54e80aaeac
contactsnazzlecreations <contact.snazzlecreations@gmail.com>
2026-08-26 00:40:44 +0200

    Activeer veilige Bieb-locaties

--- app.js
+++ app.js
@@ -154,7 +154,8 @@ const optionalModules=[
   './snazzle-input-visibility-v69.js',
   './snazzle-top-stability-v70.js',
   './snazzle-bieb-v73.js',
-  './snazzle-bieb-cloud-v74.js'
+  './snazzle-bieb-cloud-v74.js',
+  './snazzle-bieb-locations-v77.js'
 ];
 for(const modulePath of optionalModules){
   await safeImport(modulePath);
