0ba3da435ba5bfd1f292b7aa7b38cd171f43e60b
contactsnazzlecreations <contact.snazzlecreations@gmail.com>
2026-08-28 00:55:40 +0200

    Assemble recovered Snazzle card assets v133

---
+++ snazzle-card-assets-v133.js
@@ -0,0 +1,11 @@
+import w1 from './snazzle-card-asset-wild-v133-part1.js';
+import w2 from './snazzle-card-asset-wild-v133-part2.js';
+import w3 from './snazzle-card-asset-wild-v133-part3.js';
+import s1 from './snazzle-card-asset-spark-v133-part1.js';
+import s2 from './snazzle-card-asset-spark-v133-part2.js';
+import s3 from './snazzle-card-asset-spark-v133-part3.js';
+
+export const assets = {
+  wild: w1 + w2 + w3,
+  spark: s1 + s2 + s3
+};
