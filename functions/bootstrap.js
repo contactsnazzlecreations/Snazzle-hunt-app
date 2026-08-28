a32d436815b0c67f554b492989600ccc25701c39
contactsnazzlecreations <contact.snazzlecreations@gmail.com>
2026-08-28 21:14:26 +0200

    Enable dormant Special Hunts backend for real-device test

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -3,5 +3,8 @@ module.exports = {
   ...require('./index'),
   ...require('./hunt-codes'),
   ...require('./meetups'),
-  ...require('./admin-mfa')
+  ...require('./admin-mfa'),
+  ...require('./org-hunts'),
+  // Deze laatste export vervangt uitsluitend redeemOrgAccessCode door de strengere guard.
+  ...require('./org-access-guard')
 };
